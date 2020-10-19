9c62c258c8881e56bb93de811f2049228e57fa8a
Tanja Olsson <tanja.olsson@skola.botkyrka.sem>
2020-10-19 15:13:56 +0200

    ändra rad 5

--- 5/script.js
+++ 5/script.js
@@ -2,6 +2,6 @@ function areaOfTriangel (a , b) {
     a = 1,5
     b = 2,5
 }
-areaOfTriangel (a multiply b)
+areaOfTriangel (a , b)
 
 // som sagt vet ej riktigt sedan vad man gör då jag tyvärr har glömt bort
---
+++ 6/script.js
@@ -0,0 +1,5 @@
+var isSameuser (tanja.olsson)
+true = tanja.olsson
+false = tanja.olsson < 
+
+// vet ej riktigt den här heller
